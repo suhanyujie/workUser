12ca1d08c5d8829759213621bf8f6f01bf5bfad2
suhanyujie <suhanyujie@qq.com>
2022-11-01 22:59:19 +0800

    add one link for mysql collate

--- service/workUser/migration/1.add_user_table.up.sql
+++ service/workUser/migration/1.add_user_table.up.sql
@@ -6,9 +6,11 @@ CREATE TABLE `user` (
     `pwd` varchar(100) COLLATE utf8mb4_bin NOT NULL,
     `pwd_token` varchar(100) COLLATE utf8mb4_bin NOT NULL,
     `creator` int NOT NULL DEFAULT '0',
-    `updator` int NOT NULL,
+    `updater` int NOT NULL DEFAULT '0',
     `create_at` datetime NOT NULL,
     `update_at` datetime DEFAULT NULL,
     `del_flag` tinyint NOT NULL DEFAULT '0',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- 关于字符集排序规则的选用：https://blog.csdn.net/ory001/article/details/113984778
